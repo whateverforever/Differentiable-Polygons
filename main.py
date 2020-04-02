Fix gradient chaining

main.py
@@ -32,11 +32,25 @@ class Param:
     def compute(self):
         return self.factor * self.value ** self.power
 
+    def grad(self):
+        return self.factor * self.power * self.value ** (self.power-1)
+
+class Differentiable:
+    def __init__(self):
+        pass
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
         self.gradients = {}
+
+        """
+        each point has a
+        - deriv wrt. to the point it was "computed" from (through translation, rotation, etc.)
+        - deriv wrt. additional original parameters if they were used in the construction of this point
+        """
 
     def __repr__(self):
         return "Pt({:.4f},{:.4f})".format(self.x, self.y)
@@ -46,46 +60,88 @@ class Point:
         return copy.deepcopy(self.gradients)
 
     def update_grads(self, new_grads):
+        """
+        Chains new gradients onto the current ones
+        """
         pt2 = copy.deepcopy(self)
-        pt2grads = pt2.gradients
+        old_grads = pt2.gradients
+        updated_grads = {}
 
-        for param, grad in new_grads.items():
-            if param in pt2grads:
-                pt2grads[param] *= np.array(new_grads[param])
+        params = set(old_grads.keys()).union(set(new_grads.keys()))
+
+        for param in params:
+            if param == "d_prevpt":
+                continue
+
+            # we already have a trace to this parameter
+            if param in old_grads:
+                print("Trying to chain ", param)
+                # we take our gradient towards last point
+                # and the gradient of old point towards parameter
+                # d_dl = d_dprev @ dprev_dl
+                updated_grads[param] = np.array(new_grads["d_prevpt"]) @ old_grads[param]
+
+            # we don't have a trace yet, meaning we start one
             else:
-                pt2grads[param] = new_grads[param]
+                # d_dl = XXX
+                updated_grads[param] = new_grads[param]
 
+        pt2.gradients = updated_grads
         return pt2
 
-def translate2(pt, vec):
-    if isinstance(vec[0], Number):
-        return translate_const(pt, vec)
-    elif isinstance(vec[0], Param):
-        return translate_param1(pt, vec)
+Vector = Point
 
-def translate_const(pt, vec):
-    pass
-
-def translate_param1(pt, vec):
-    param = vec[0]
-
-    deltax = param.compute()
-    deltay = vec[1]
+def translate(pt, vec):
+    deltax = vec[0].compute() if isinstance(vec[0], Param) else vec[0]
+    deltay = vec[1].compute() if isinstance(vec[1], Param) else vec[1]
 
     x2 = pt.x + deltax
     y2 = pt.y + deltay
 
     _grads = {}
-    _grads[param.name] = [
-        [1],
-        [0]
-    ]
+    if isinstance(vec[0], Param):
+        _grads[vec[0].name] = [
+            [vec[0].grad()],
+            [0]
+        ]
+    
+    if isinstance(vec[1], Param):
+        _grads[vec[1].name] = [
+            [0],
+            [vec[1].grad()]
+        ]
+
+    d_prevpt = np.array([
+        [1, 0],
+        [0, 1]
+    ])
+
+    _grads["d_prevpt"] = d_prevpt
 
     pt2 = pt.update_grads(_grads)
     pt2.x = x2
     pt2.y = y2
 
     return pt2
+
+def norm(pt):
+    params = list(pt.grads.keys())
+
+    l2_norm = np.sqrt(pt.x**2 + pt.y ** 2)
+
+    grad = [[pt.x/np.sqrt(pt.x**2 + pt.y**2), pt.y/np.sqrt(pt.x**2 + pt.y**2)]]
+
+    grads = {}
+    grads["d_prevpt"] = grad
+
+    print("new grads:", grads)
+
+    # Point isnt the right class. Should Differentiable or Scalar or Thing or sth
+    length = pt.update_grads(grads)
+    length.x = l2_norm
+    length.y = 0
+
+    return length
 
 def rotate_param(pt, origin, angle_param):
     x1 = pt.x
@@ -111,6 +167,10 @@ def rotate_param(pt, origin, angle_param):
 
     _grads = {}
     _grads[angle_param.name] = dangle
+    _grads["d_prevpt"] = np.array([
+        [np.cos(angle), -np.sin(angle)],
+        [np.sin(angle), np.cos(angle)]
+    ])
 
     pt2 = pt.update_grads(_grads)
     pt2.x = x2
@@ -134,9 +194,27 @@ def main():
 
         pt = Point(0, 0)
         
-        pt2 = translate2(pt, [l, 0])
+        pt2 = translate(pt, [l, 0])
         pt3 = rotate_param(pt2, pt, theta)
-        pt4 = translate2(pt3, [2*l, 0])
+        pt4 = translate(pt3, [2*l, 0])
+
+        #length = norm(pt4)
+        #print(length)
+
+        """
+        A = diff_vec = pt4 - pt2
+        dA/dparams = pt4.grads() - pt2.grads()
+
+        B = diff = diff_vec.length()
+        dB/dparams = dlength/dA * dA/dparams
+        
+        translate(pt4, [diff * l, 0])  # diff * l is a Scalar/Point * param
+            diff.compute() * l.compute()
+
+            if other is param:
+                per parameter:
+                    self.grad[parameter] *= other.grad() * self.compute() + self.grad() * other.compute()
+        """
 
         return pt4, pt4.grads
 