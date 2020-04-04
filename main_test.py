5ba35a4d87ebf015c5219557f3d28f9b2b7ae1c2
max <max@thousandyardstare.de>
2020-04-04 13:16:46 +0200

    Add optimization test case

--- main_test.py
+++ main_test.py
@@ -1,6 +1,60 @@
 import unittest as ut
 import numpy as np  # type:ignore
-from main import translate, Param, Point, Line  # type:ignore
+from main import (
+    translate,
+    rotate_param,
+    diffvec,
+    norm,
+    Param,
+    Point,
+    Line,
+)  # type:ignore
+
+
+class TestOverall(ut.TestCase):
+    def test_norm_optimization(self):
+        from scipy import optimize  # type: ignore
+
+        def parametric_pt(l=2.0, theta=np.radians(60)):
+            l = Param("l", l)
+            theta = Param("theta", theta)
+
+            pt = Point(0, 0)
+
+            pt2 = translate(pt, [l, 0])
+            pt3 = rotate_param(pt2, pt, theta)
+            pt4 = translate(pt3, [2 * l, 0])
+
+            diff_vec = diffvec(pt4, Point(8, 2))
+
+            length = norm(diff_vec)
+
+            return length, length.grads
+
+        def f(x):
+            l, theta = x
+            scl, _ = parametric_pt(l, theta)
+            dist = scl.value
+
+            return dist
+
+        def jac(x):
+            l, theta = x
+            _, pt_grads = parametric_pt(l, theta)
+
+            grads = []
+            for param in ["l", "theta"]:
+                grads.append(pt_grads[param])
+
+            return np.squeeze(grads)
+
+        x0 = [2.0, np.radians(60)]
+        xs = []
+
+        res = optimize.minimize(f, x0, method="L-BFGS-B", jac=jac)
+        length_reached, _ = parametric_pt(*res.x)
+
+        assert np.isclose(length_reached.value, 0)
 
 
 class TestTranslate(ut.TestCase):
