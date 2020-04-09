a0527c6108e823b334b963d200ecdf50d0f4aed1
max <max@thousandyardstare.de>
2020-04-09 10:42:54 +0200

    Add slits to example

--- example1.py
+++ example1.py
@@ -5,6 +5,7 @@ from main import Line, Vector, Point, Scalar, Param
 
 
 def main():
+    s = Param("s", 0.05)  # width of cuts
     t = Param("t", 0.15)
     l = Param("l", 2.0)
     theta = Param("theta", np.radians(10))
@@ -32,9 +33,12 @@ def main():
 
     vec_left_up = (corner_top - corner_left) / (corner_top - corner_left).norm()
     vec_bottom_left = (corner_left - corner_right) / (corner_left - corner_right).norm()
+    vec_right_down = (corner_right - corner_top) / (corner_right - corner_top).norm()
 
     pt1 = corner_left + vec_left_up * t
     pt2 = corner_right + vec_bottom_left * t
+    pt3 = corner_top + vec_right_down * t
+
     ax.plot(pt1.x, pt1.y, "o")
 
     assert pt1.grads["lower_angles"][0][0] < 0
@@ -42,16 +46,35 @@ def main():
 
     # TODO: Check if gradients for d_dpt1 and d_dpt2 correct (pivot!)
     cut_lower = line_horiz.translate(vec_left_up * t).rotate_ccw(theta, pivot=pt1)
+    cut_lower2 = line_horiz.rotate_ccw(theta, pivot=corner_left).translate(
+        vec_left_up * (t + s)
+    )
+
     cut_right = line_right.translate(vec_bottom_left * t).rotate_ccw(theta, pivot=pt2)
+    cut_right2 = line_right.rotate_ccw(theta, pivot=corner_right).translate(
+        vec_bottom_left * (t + s)
+    )
 
-    cut_lower.plot(ax=ax, lims=lims)
-    cut_right.plot(ax=ax, lims=lims)
+    cut_top = line_left.translate(vec_right_down * t).rotate_ccw(theta, pivot=pt3)
+    cut_top2 = line_left.rotate_ccw(theta, pivot=corner_top).translate(
+        vec_right_down * (t + s)
+    )
 
-    pt3 = cut_lower.intersect(line_right)
-    ax.plot(pt3.x, pt3.y, "o")
+    cut_lower.plot(ax=ax, lims=lims, label="lower")
+    cut_lower2.plot(ax=ax, lims=lims, label="lowe2")
+    cut_right.plot(ax=ax, lims=lims, label="right")
+    cut_right2.plot(ax=ax, lims=lims, label="right2")
+    cut_top.plot(ax=ax, lims=lims, label="top")
+    cut_top2.plot(ax=ax, lims=lims, label="top2")
 
-    print("pt3 grads", pt3.grads)
+    # pt3 = cut_lower.intersect(line_right)
+    # ax.plot(pt3.x, pt3.y, "o")
 
+    # print("pt3 grads", pt3.grads)
+
+    plt.legend()
+    plt.xlim((-0.05, 2.05))
+    plt.ylim((-0.05, 1.8))
     plt.show()
 
 
