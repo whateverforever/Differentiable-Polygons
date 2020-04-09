63bba4136ac412439ce430977512af27a3755381
max <max@thousandyardstare.de>
2020-04-09 10:29:45 +0200

    Fix example 1 for correct cut_right

--- example1.py
+++ example1.py
@@ -10,10 +10,10 @@ def main():
     theta = Param("theta", np.radians(10))
     lower_angles = Param("lower_angles", np.radians(60))
 
-    origin = Point(0, 0)
-    pt1 = Point(l, 0)
+    corner_left = Point(0, 0)
+    corner_right = Point(l, 0)
 
-    line_horiz = Line.make_from_points(origin, pt1)
+    line_horiz = Line.make_from_points(corner_left, corner_right)
     line_left = line_horiz.rotate_ccw(lower_angles)
     line_right = line_horiz.rotate_ccw(-lower_angles).translate(Vector(l, 0))
 
@@ -30,17 +30,24 @@ def main():
     assert np.allclose(corner_top.grads["lower_angles"][0], [0.0])
     assert corner_top.grads["lower_angles"][1][0] > 0
 
-    vec_left_up = (corner_top - origin) / (corner_top - origin).norm()
-    pt2 = vec_left_up * t
-    ax.plot(pt2.x, pt2.y, "o")
+    vec_left_up = (corner_top - corner_left) / (corner_top - corner_left).norm()
+    vec_bottom_left = (corner_left - corner_right) / (corner_left - corner_right).norm()
 
-    assert pt2.grads["lower_angles"][0][0] < 0
-    assert pt2.grads["lower_angles"][1][0] > 0
+    pt1 = corner_left + vec_left_up * t
+    pt2 = corner_right + vec_bottom_left * t
+    ax.plot(pt1.x, pt1.y, "o")
 
-    line_cut = line_horiz.translate(pt2).rotate_ccw(theta)
-    line_cut.plot(ax=ax, lims=lims)
+    assert pt1.grads["lower_angles"][0][0] < 0
+    assert pt1.grads["lower_angles"][1][0] > 0
 
-    pt3 = line_cut.intersect(line_right)
+    # TODO: Check if gradients for d_dpt1 and d_dpt2 correct (pivot!)
+    cut_lower = line_horiz.translate(vec_left_up * t).rotate_ccw(theta, pivot=pt1)
+    cut_right = line_right.translate(vec_bottom_left * t).rotate_ccw(theta, pivot=pt2)
+
+    cut_lower.plot(ax=ax, lims=lims)
+    cut_right.plot(ax=ax, lims=lims)
+
+    pt3 = cut_lower.intersect(line_right)
     ax.plot(pt3.x, pt3.y, "o")
 
     print("pt3 grads", pt3.grads)
