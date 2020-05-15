2c9a7c9613463269cd2cec9bfa09b046699054fd
max <max@thousandyardstare.de>
2020-05-15 15:46:29 +0200

    Fix imports for example1 tests

--- example1_test.py
+++ example1_test.py
@@ -1,5 +1,11 @@
-from example1 import main, create_unit_cell
+from .example1 import main, create_unit_cell
+from scipy.optimize import check_grad
 
 
 def test_performance(benchmark):
-    result = benchmark(main)
+    # result = benchmark(main)
+    pass
+
+
+def test_gradient():
+    pass
