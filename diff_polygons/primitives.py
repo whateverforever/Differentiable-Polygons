Fixed gradients in Point.rotate, thanks Hypothesis!

diff_polygons/primitives.py
@@ -75,7 +75,7 @@ class Scalar(GradientCarrier):
     def __add__(scal1: Scalar, scal2: Scalar) -> Scalar:
         scal1 = Scalar(scal1)
         scal2 = Scalar(scal2)
-        
+
         val_new = scal1.value + scal2.value
 
         inputs = {"scal1": scal1, "scal2": scal2}
@@ -266,9 +266,9 @@ class Point(GradientCarrier):
 
         return Scalar(l2_norm).with_grads_from_previous(inputs, _grads)
 
-    def rotate(pt: Point, origin: Point, angle_param: Scalar) -> Point:
+    def rotate(pt: Point, origin: Point, angle_rad: Scalar) -> Point:
         # TODO: Same for points, coercion
-        angle_param = Scalar(angle_param)
+        angle_rad = Scalar(angle_rad)
 
         x1 = pt.x
         y1 = pt.y
@@ -276,7 +276,7 @@ class Point(GradientCarrier):
         ox = origin.x
         oy = origin.y
 
-        angle = angle_param.value
+        angle = angle_rad.value
 
         x2 = (x1 - ox) * np.cos(angle) - (y1 - oy) * np.sin(angle) + ox
         y2 = (x1 - ox) * np.sin(angle) + (y1 - oy) * np.cos(angle) + oy
@@ -289,16 +289,18 @@ class Point(GradientCarrier):
             [-np.sin(angle), -np.cos(angle) + 1],
         ]
         d_dangle = [
-            [(x1 - ox) * (-np.sin(angle) - (y1 - oy) * np.cos(angle))],
-            [(x1 - ox) * np.cos(angle) + (y1 - oy) * (-np.sin(angle))],
+            [(oy - y1) * np.cos(angle) + (ox - x1) * np.sin(angle)],
+            [(-ox + x1) * np.cos(angle) + (oy - y1) * np.sin(angle)],
         ]
 
-        inputs = {"pt": pt, "origin": origin, "angle": angle_param}
+        inputs = {"pt": pt, "origin": origin, "angle": angle_rad}
 
         _grads = {}
         _grads["pt"] = d_dpt
         _grads["origin"] = d_dorigin
         _grads["angle"] = d_dangle
+
+        print("local grads", _grads["angle"])
 
         pt2 = Point(x2, y2).with_grads_from_previous(inputs, _grads)
 

diff_polygons/primitives_test.py
@@ -48,6 +48,16 @@ def test_update_grads():
 
 
 class TestScalar:
+    @given(reals)
+    def test_coercion(self, val):
+        a = Param("a", val)
+        b = Scalar(a)
+        c = Scalar(val)
+
+        assert b.value == a.value
+        assert b.value == c.value
+        assert b.grads == a.grads
+
     def test_make_param(self):
         l = Scalar.Param("l", 2.0)
 
@@ -236,10 +246,10 @@ class TestPoint:
         assert np.isclose(np.sqrt(2), pt2.y)
 
     @given(
-        reals2(min_value=-1000, max_value=1000),
-        reals2(min_value=-1000, max_value=1000),
-        reals2(min_value=-1000, max_value=1000),
-        reals2(min_value=-1000, max_value=1000),
+        reals2(min_value=-100, max_value=100),
+        reals2(min_value=-100, max_value=100),
+        reals2(min_value=-100, max_value=100),
+        reals2(min_value=-100, max_value=100),
         reals2(min_value=-720, max_value=720),
     )
     def test_rotation_grad(self, x1, y1, x2, y2, angle):
@@ -255,10 +265,10 @@ class TestPoint:
             return pt1.rotate(pt2, angle)
 
         x1 = Param("x1", x1)
-        x2 = Param("x2", x2)
         y1 = Param("y1", y1)
+        x2 = Param("x2", x2)
         y2 = Param("y2", y2)
-        angle = Param("angle", angle)
+        angle = Param("angle", np.radians(angle))
         check_all_grads(f, [x1, y1, x2, y2, angle])
 
     @given(reals, reals, reals, reals)